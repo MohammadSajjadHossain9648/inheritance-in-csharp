a992728baef9cc142d8a6781ef4ca14a7676c0c0
MohammadSajjadHossain9648 <sajjadhossain9648@gmail.com>
2022-02-07 21:48:14 +0600

    final commit

---
+++ Chef.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Chef
+    {
+        public void makeChicken()
+        {
+            Console.WriteLine("i can make chicken.");
+        }
+        public void makeBurger()
+        {
+            Console.WriteLine("i can make burger.");
+        }
+        public virtual void specialDish() 
+        {
+            Console.WriteLine("i can make special bbq rice.");
+        }
+    }
+}
---
+++ Italianchef.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Italianchef : Chef
+    {
+        public override void specialDish()
+        {
+            Console.WriteLine("i can make special bbq ribs.");
+        }
+    }
+}
